7c54ba5ca0c35feacbc72b79e1cc8dcc85e94d6c
son.tran <son.tran@team.enouvo.com>
2023-12-21 15:01:38 +0700

    feat(master): add signs color

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -8,7 +8,27 @@ return {
         current_line_blame_opts = {
           delay = 100,
         },
+        sign_priority = 5,
+        signs = {
+          add = { hl = "GitSignsAdd", text = "|", numhl = "GitSignsAddNr", linehl = "GitSignsAdd" },
+          change = { hl = "GitSignsChange", text = "|", numhl = "GitSignsChangeNr", linehl = "GitSignsChange" },
+          delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDelete" },
+          topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDelete" },
+          changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChange" },
+        },
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+        end,
       })
+      vim.api.nvim_set_hl(0, "GitSignsAdd", { ctermbg = 0, fg = "#50FA7B" })
+      vim.api.nvim_set_hl(0, "GitSignsChange", { ctermbg = 0, fg = "#FFB86C" })
+      vim.api.nvim_set_hl(0, "GitSignsDelete", { ctermbg = 0, fg = "#FF5555" })
     end,
   },
 }
