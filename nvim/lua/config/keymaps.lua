22a16f803f37ecddaf314edf962d8542262e7e83
son.tran <son.tran@team.enouvo.com>
2023-12-21 17:45:07 +0700

    feat(master): add new keymaps for git conflict

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,16 +1,16 @@
-local keymap = vim.keymap.set
+local map = vim.keymap.set
 
 -- Rewrite ControlKeys
-keymap("n", "+", "<C-a>")
-keymap("n", "-", "<C-x>")
+map("n", "+", "<C-a>")
+map("n", "-", "<C-x>")
 
 -- Select all
-keymap("n", "<C-a>", "gg<S-v>G")
+map("n", "<C-a>", "gg<S-v>G")
 
 -- Copy
-keymap("n", "<C-c>", "yy")
-keymap("v", "<C-c>", "y")
-keymap("n", "<C-v>", "p")
+map("n", "<C-c>", "yy")
+map("v", "<C-c>", "y")
+map("n", "<C-v>", "p")
 
 -- Google Search
 local searching_google_in_normal =
@@ -18,10 +18,18 @@ local searching_google_in_normal =
 local searching_google_in_visual =
   [[<ESC>gv"gy<ESC>:lua vim.fn.system({'open', 'https://google.com/search?q=' .. vim.fn.getreg('g')})<CR>]]
 
-keymap("n", "<C-g>", searching_google_in_normal, { silent = true, noremap = true })
-keymap("v", "<C-g>", searching_google_in_visual, { silent = true, noremap = true })
+map("n", "<C-g>", searching_google_in_normal, { silent = true, noremap = true })
+map("v", "<C-g>", searching_google_in_visual, { silent = true, noremap = true })
 
 -- Open folder on current buffer
 local open_folder = [[:lua vim.fn.system({ 'open', vim.fn.expand("<cfile")})<CR>]]
 
-keymap("n", "<leader>o", open_folder, { silent = true, noremap = true })
+map("n", "<leader>o", open_folder, { silent = true, noremap = true })
+
+-- Git conflict
+map("n", "<leader>cb", "<Plug>(git-conflict-both)")
+map("n", "<leader>c0", "<Plug>(git-conflict-none)")
+map("n", "<leader>ct", "<Plug>(git-conflict-theirs)")
+map("n", "<leader>cn", "<Plug>(git-conflict-next-conflict)")
+map("n", "<leader>cp", "<Plug>(git-conflict-prev-conflict)")
+map("n", "<leader>gq", ":GitConflictListQf <CR>")
--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     -- import/override with your plugins
     { import = "plugins" },
+    { "akinsho/git-conflict.nvim", version = "*", config = true },
   },
   defaults = {
     lazy = false,
