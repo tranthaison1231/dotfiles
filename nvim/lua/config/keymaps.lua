0b68341f67e8a36d358a0965da5b45158e01cfe4
son.tran <son.tran@team.enouvo.com>
2023-12-22 10:58:22 +0700

    feat(master): update key maps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -25,11 +25,3 @@ map("v", "<C-g>", searching_google_in_visual, { silent = true, noremap = true })
 local open_folder = [[:lua vim.fn.system({ 'open', vim.fn.expand("<cfile")})<CR>]]
 
 map("n", "<leader>o", open_folder, { silent = true, noremap = true })
-
--- Git conflict
-map("n", "<leader>cb", "<Plug>(git-conflict-both)")
-map("n", "<leader>c0", "<Plug>(git-conflict-none)")
-map("n", "<leader>ct", "<Plug>(git-conflict-theirs)")
-map("n", "<leader>cn", "<Plug>(git-conflict-next-conflict)")
-map("n", "<leader>cp", "<Plug>(git-conflict-prev-conflict)")
-map("n", "<leader>gq", ":GitConflictListQf <CR>")
