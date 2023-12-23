fab0462f7a3d36e89612fe674d9c388246eadfb7
son.tran <son.tran@team.enouvo.com>
2023-12-23 14:31:33 +0700

    feat(master): add git conflict just lazy inside git repo

--- nvim/lua/plugins/git-conflict.lua
+++ nvim/lua/plugins/git-conflict.lua
@@ -1,6 +1,13 @@
+-- Only load those plugins if the current buffer is a git repository
+local is_inside_git_repo = function()
+  local git_dir = vim.fn.finddir(".git", vim.fn.expand("%:p:h") .. ";")
+  return vim.fn.isdirectory(git_dir) ~= 0
+end
+
 return {
   "akinsho/git-conflict.nvim",
   version = "*",
+  lazy = not is_inside_git_repo(),
   config = function()
     vim.cmd([[highlight Incoming guibg=#6272A4]])
 
