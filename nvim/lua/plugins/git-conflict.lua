ca9e9a90de1dd9d9b5d8900bb9150b9949f93819
son.tran <son.tran@team.enouvo.com>
2023-12-23 14:37:40 +0700

    feat(master): add debug false on git conflict

--- nvim/lua/plugins/git-conflict.lua
+++ nvim/lua/plugins/git-conflict.lua
@@ -13,6 +13,7 @@ return {
 
     require("git-conflict").setup({
       default_mappings = true, -- disable buffer local mapping created by this plugin
+      debug = false,
       default_commands = true, -- disable commands created by this plugin
       disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
       list_opener = "copen",
