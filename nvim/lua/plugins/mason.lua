8b2c42b16478b574ca264efa6113c7a0157f7c9f
son.tran <son.tran@team.enouvo.com>
2024-01-13 09:01:06 +0700

    feat(master): update cspell

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,24 +1,18 @@
 return {
-  -- cmdline tools and lsp servers
   {
     "williamboman/mason.nvim",
     lazy = false,
     opts = {
       ensure_installed = {
         "stylua",
-        -- formatters
         "eslint_d",
         "prettier",
         "prettierd",
-        -- code spell
         "codespell",
         "misspell",
         "cspell",
-        -- markdown
         "markdownlint",
-        -- astro
         "astro-language-server",
-        -- Solidity
         "solidity",
       },
     },
