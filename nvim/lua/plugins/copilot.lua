7a745d0d3a05defd33311ae8500185fa9a2dcd16
son.tran <son.tran@team.enouvo.com>
2024-01-17 00:34:28 +0700

    feat(master): update copilot

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -7,6 +7,23 @@ local function fg(name)
   return fg and { fg = string.format("#%06x", fg) } or nil
 end
 
+local function get_clients(opts)
+  local ret = {} ---@type lsp.Client[]
+  if vim.lsp.get_clients then
+    ret = vim.lsp.get_clients(opts)
+  else
+    ---@diagnostic disable-next-line: deprecated
+    ret = vim.lsp.get_active_clients(opts)
+    if opts and opts.method then
+      ---@param client lsp.Client
+      ret = vim.tbl_filter(function(client)
+        return client.supports_method(opts.method, { bufnr = opts.bufnr })
+      end, ret)
+    end
+  end
+  return opts and opts.filter and vim.tbl_filter(opts.filter, ret) or ret
+end
+
 return {
   {
     "zbirenbaum/copilot.lua",
@@ -42,7 +59,7 @@ return {
           if not package.loaded["copilot"] then
             return
           end
-          local ok, clients = pcall(require("lazyvim.util").lsp.get_clients, { name = "copilot", bufnr = 0 })
+          local ok, clients = pcall(get_clients, { name = "copilot", bufnr = 0 })
           if not ok then
             return false
           end
