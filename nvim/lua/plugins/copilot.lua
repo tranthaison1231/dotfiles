9301e897456b7fd1af4e82deeb81118e80dec490
son.tran <son.tran@team.enouvo.com>
2024-01-17 00:25:06 +0700

    feat(master): update config

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,3 +1,12 @@
+local function fg(name)
+  ---@type {foreground?:number}?
+  ---@diagnostic disable-next-line: deprecated
+  local hl = vim.api.nvim_get_hl and vim.api.nvim_get_hl(0, { name = name }) or vim.api.nvim_get_hl_by_name(name, true)
+  ---@diagnostic disable-next-line: undefined-field
+  local fg = hl and (hl.fg or hl.foreground)
+  return fg and { fg = string.format("#%06x", fg) } or nil
+end
+
 return {
   {
     "zbirenbaum/copilot.lua",
@@ -17,12 +26,11 @@ return {
     optional = true,
     event = "VeryLazy",
     opts = function(_, opts)
-      local Util = require("lazyvim.util")
       local colors = {
-        [""] = Util.ui.fg("Special"),
-        ["Normal"] = Util.ui.fg("Special"),
-        ["Warning"] = Util.ui.fg("DiagnosticError"),
-        ["InProgress"] = Util.ui.fg("DiagnosticWarn"),
+        [""] = fg("Special"),
+        ["Normal"] = fg("Special"),
+        ["Warning"] = fg("DiagnosticError"),
+        ["InProgress"] = fg("DiagnosticWarn"),
       }
       table.insert(opts.sections.lualine_x, 2, {
         function()
--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,4 +1,14 @@
-local Util = require("lazyvim.util")
+local function format(opts)
+  opts = vim.tbl_deep_extend("force", {}, opts or {}, require("lazyvim.util").opts("nvim-lspconfig").format or {})
+  local ok, conform = pcall(require, "conform")
+  if ok then
+    opts.formatters = {}
+    opts.lsp_fallback = true
+    conform.format(opts)
+  else
+    vim.lsp.buf.format(opts)
+  end
+end
 
 return {
   {
@@ -6,27 +16,30 @@ return {
     event = "LazyFile",
     dependencies = { "mason.nvim" },
     init = function()
-      Util.on_very_lazy(function()
-        require("lazyvim.util").format.register({
-          name = "none-ls.nvim",
-          priority = 200, -- set higher than conform, the builtin formatter
-          primary = true,
-          format = function(buf)
-            return Util.lsp.format({
-              bufnr = buf,
-              filter = function(client)
-                return client.name == "null-ls"
-              end,
-            })
-          end,
-          sources = function(buf)
-            local ret = require("null-ls.sources").get_available(vim.bo[buf].filetype, "NULL_LS_FORMATTING") or {}
-            return vim.tbl_map(function(source)
-              return source.name
-            end, ret)
-          end,
-        })
-      end)
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "VeryLazy",
+        callback = function()
+          require("lazyvim.util").format.register({
+            name = "none-ls.nvim",
+            priority = 200, -- set higher than conform, the builtin formatter
+            primary = true,
+            format = function(buf)
+              return format({
+                bufnr = buf,
+                filter = function(client)
+                  return client.name == "null-ls"
+                end,
+              })
+            end,
+            sources = function(buf)
+              local ret = require("null-ls.sources").get_available(vim.bo[buf].filetype, "NULL_LS_FORMATTING") or {}
+              return vim.tbl_map(function(source)
+                return source.name
+              end, ret)
+            end,
+          })
+        end,
+      })
     end,
     opts = function(_, opts)
       local nls = require("null-ls")
