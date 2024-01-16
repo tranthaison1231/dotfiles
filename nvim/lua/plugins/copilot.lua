d4ac78065167edc91192938fcde35f5e6384ab80
son.tran <son.tran@team.enouvo.com>
2024-01-17 00:30:24 +0700

    feat(master): update nvim

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -68,11 +68,14 @@ return {
         config = function(_, opts)
           local copilot_cmp = require("copilot_cmp")
           copilot_cmp.setup(opts)
-          require("lazyvim.util").lsp.on_attach(function(client)
-            if client.name == "copilot" then
-              copilot_cmp._on_insert_enter({})
-            end
-          end)
+          vim.api.nvim_create_autocmd("LspAttach", {
+            callback = function(args)
+              local client = vim.lsp.get_client_by_id(args.data.client_id)
+              if client.name == "copilot" then
+                copilot_cmp._on_insert_enter({})
+              end
+            end,
+          })
         end,
       },
     },
