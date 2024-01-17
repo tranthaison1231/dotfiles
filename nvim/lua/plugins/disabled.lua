a8fb3b8e14adc8047b669e567210ddd2504b94fc
son.tran <son.tran@team.enouvo.com>
2024-01-17 14:41:30 +0700

    feat(master): update plugin to disabled

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -5,4 +5,6 @@ return {
   { "nvim-treesitter/nvim-treesitter-context", enabled = false },
   { "echasnovski/mini.starter", enabled = false },
   { "folke/neoconf.nvim", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
+  { "ellisonleao/gruvbox.nvim", enabled = false },
 }
