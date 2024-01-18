c6c195eef128f997d246bca321f1363a80ce51cb
son.tran <son.tran@team.enouvo.com>
2024-01-18 09:04:46 +0700

    feat(master): add lualine

--- nvim-new/lua/config/options.lua
+++ nvim-new/lua/config/options.lua
@@ -46,12 +46,4 @@ opt.virtualedit = "block" -- Allow cursor to move where there is no text in visu
 opt.wildmode = "longest:full,full" -- Command-line completion mode
 opt.winminwidth = 5 -- Minimum window width
 opt.wrap = false -- Disable line wrap
-opt.fillchars = {
-	foldopen = "",
-	foldclose = "",
-	-- fold = "⸱",
-	fold = " ",
-	foldsep = " ",
-	diff = "╱",
-	eob = " ",
-}
+
--- nvim-new/lua/plugins/lualine.lua
+++ nvim-new/lua/plugins/lualine.lua
@@ -1,4 +1,27 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "meuter/lualine-so-fancy.nvim",
+    },
+    opts = {
+      options = {
+        theme = "dracula",
+      },
+      sections = {
+        lualine_a = { { "fancy_mode", width = 3 } },
+        lualine_b = { { "fancy_branch" }, { "fancy_diff" } },
+        lualine_c = { { "fancy_cwd", substitute_home = true } },
+        lualine_x = {
+          { "fancy_macro" },
+          { "fancy_diagnostics" },
+          { "fancy_searchcount" },
+          { "fancy_location" },
+        },
+        lualine_y = { { "fancy_filetype", ts_icon = "" } },
+      },
+    },
+  },
 }
+
