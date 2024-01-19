120eeba05ce59ebfbe0aa91d39bcc389b492d279
son.tran <son.tran@team.enouvo.com>
2024-01-19 09:09:20 +0700

    feat(master): update neovim config new

--- nvim-new/lua/plugins/none-ls.lua
+++ nvim-new/lua/plugins/none-ls.lua
@@ -11,6 +11,7 @@ return {
           }),
           null_ls.builtins.formatting.prettierd,
           null_ls.builtins.diagnostics.eslint_d,
+          null_ls.builtins.diagnostics.markdownlint,
         },
         on_attach = on_attach,
       })
