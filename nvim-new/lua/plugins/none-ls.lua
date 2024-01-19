d8bd6749afa2d9f1b035e1ae0971dfd92a1baeec
son.tran <son.tran@team.enouvo.com>
2024-01-19 08:17:30 +0700

    feat(master): update neovim new

---
+++ nvim-new/lua/plugins/none-ls.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "nvimtools/none-ls.nvim",
+    config = function()
+      local null_ls = require("null-ls")
+      local on_attach = require("lsp/on_attach")
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua.with({
+            extra_args = { "config-path", "{$HOME}/.config/nvim/lua/stylua.toml" },
+          }),
+          null_ls.builtins.formatting.prettierd,
+          null_ls.builtins.diagnostics.eslint_d,
+        },
+        on_attach = on_attach,
+      })
+    end,
+  },
+}
