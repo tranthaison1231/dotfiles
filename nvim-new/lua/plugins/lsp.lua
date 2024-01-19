4267abc785e57fcf4d0a828def3bd6774d7dae80
son.tran <son.tran@team.enouvo.com>
2024-01-19 09:05:47 +0700

    feat(master): update lsp

--- nvim-new/lua/plugins/lsp.lua
+++ nvim-new/lua/plugins/lsp.lua
@@ -9,30 +9,82 @@ return {
     "nvimdev/lspsaga.nvim",
     event = "LspAttach",
     config = function()
-     local saga = require("lspsaga")
+      local saga = require("lspsaga")
 
-     saga.setup({
-       symbol_in_winbar = {
-        enable = false,
-       },
-      ui = {
-       border = "rounded",
-      },
-      beacon = {
-       enable = false,
-      },
-      rename = {
-       quit = "<C-c>",
-       exec = "<CR>",
-       mark = "x",
-       confirm = "<CR>",
-       in_select = false,
-     },
-    }) 
+      saga.setup({
+        symbol_in_winbar = {
+          enable = false,
+        },
+        ui = {
+          border = "rounded",
+        },
+        beacon = {
+          enable = false,
+        },
+        rename = {
+          quit = "<C-c>",
+          exec = "<CR>",
+          mark = "x",
+          confirm = "<CR>",
+          in_select = false,
+        },
+      })
     end,
     dependencies = {
       { "nvim-tree/nvim-web-devicons" },
       { "nvim-treesitter/nvim-treesitter" },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    version = false, -- last release is way too old and doesn't work on Windows
+    build = ":TSUpdate",
+    event = { "VeryLazy" },
+    opts = {
+      highlight = { enable = true },
+      indent = { enable = true },
+      ensure_installed = {
+        "bash",
+        "c",
+        "diff",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+      textobjects = {
+        move = {
+          enable = true,
+          goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+          goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+          goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+          goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+        },
+      },
     },
   },
   {
