f44bda1fb4347a07c44c86d86278825f564fa015
son.tran <son.tran@team.enouvo.com>
2024-01-23 22:07:55 +0700

    feat(master): update treesister

--- nvim-new/lua/plugins/treesitter.lua
+++ nvim-new/lua/plugins/treesitter.lua
@@ -1,9 +1,33 @@
 return {
-  { "nvim-treesitter/nvim-treesitter-textobjects" },
   {
     "nvim-treesitter/nvim-treesitter",
     version = false,
     build = ":TSUpdate",
+    dependencies = {
+      {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        config = function()
+          local move = require("nvim-treesitter.textobjects.move") ---@type table<string,fun(...)>
+          local configs = require("nvim-treesitter.configs")
+          for name, fn in pairs(move) do
+            if name:find("goto") == 1 then
+              move[name] = function(q, ...)
+                if vim.wo.diff then
+                  local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
+                  for key, query in pairs(config or {}) do
+                    if q == query and key:find("[%]%[][cC]") then
+                      vim.cmd("normal! " .. key)
+                      return
+                    end
+                  end
+                end
+                return fn(q, ...)
+              end
+            end
+          end
+        end,
+      },
+    },
     config = function()
       require("nvim-treesitter.configs").setup({
         highlight = { enable = true },
