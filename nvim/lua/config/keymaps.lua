59c140fceaed90552091f2e19abb2d236c620056
son.tran <son.tran@team.enouvo.com>
2024-01-23 22:32:58 +0700

    feat(master): add trouble

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -60,3 +60,8 @@ map("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
 map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
 map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
 map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
+
+-- Trouble
+map("n", "<leader>xx", function()
+  require("trouble").toggle()
+end)
