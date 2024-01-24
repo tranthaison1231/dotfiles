7c4aeaf4d233f1ebdec7d2bf39eb7b57550a3530
son.tran <son.tran@team.enouvo.com>
2024-01-24 11:08:44 +0700

    feat(master): update lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -8,18 +8,36 @@ return {
     opts = {
       options = {
         theme = "dracula",
+        component_separators = { left = "│", right = "│" },
+        section_separators = { left = "", right = "" },
+        globalstatus = true,
+        refresh = {
+          statusline = 100,
+        },
       },
       sections = {
-        lualine_a = { { "fancy_mode", width = 3 } },
-        lualine_b = { { "fancy_branch" }, { "fancy_diff" } },
-        lualine_c = { { "fancy_cwd", substitute_home = true } },
+        lualine_a = {
+          { "fancy_mode", width = 3 },
+        },
+        lualine_b = {
+          { "fancy_branch" },
+          { "fancy_diff" },
+        },
+        lualine_c = {
+          { "fancy_cwd", substitute_home = true },
+        },
         lualine_x = {
           { "fancy_macro" },
           { "fancy_diagnostics" },
           { "fancy_searchcount" },
           { "fancy_location" },
         },
-        lualine_y = { { "fancy_filetype", ts_icon = "" } },
+        lualine_y = {
+          { "fancy_filetype", ts_icon = "" },
+        },
+        lualine_z = {
+          { "fancy_lsp_servers" },
+        },
       },
     },
   },
