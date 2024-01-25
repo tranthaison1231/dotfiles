faa19ac48e050ad4b979d14e2462892ffcf79ed3
son.tran <son.tran@team.enouvo.com>
2024-01-25 15:15:03 +0700

    feat(master): update copilot cmp

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,69 +1,63 @@
 return {
-  "saadparwaiz1/cmp_luasnip",
-  {
-    "hrsh7th/cmp-nvim-lua",
-    dependencies = "cmp_luasnip",
-  },
-  {
-    "hrsh7th/cmp-nvim-lsp",
-    dependencies = "cmp-nvim-lua",
-  },
-  {
-    "hrsh7th/cmp-buffer",
-    dependencies = "cmp-nvim-lsp",
-  },
-  {
-    "hrsh7th/cmp-path",
-    dependencies = "cmp-nvim-lsp",
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      {
-        "rafamadriz/friendly-snippets",
-        config = function()
-          require("luasnip.loaders.from_vscode").lazy_load()
-        end,
-      },
-    },
-    opts = {
-      history = true,
-      delete_check_events = "TextChanged",
-    },
-    keys = {
-      {
-        "<tab>",
-        function()
-          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
-        end,
-        expr = true,
-        silent = true,
-        mode = "i",
-      },
-      {
-        "<tab>",
-        function()
-          require("luasnip").jump(1)
-        end,
-        mode = "s",
-      },
-      {
-        "<s-tab>",
-        function()
-          require("luasnip").jump(-1)
-        end,
-        mode = { "i", "s" },
-      },
-    },
-  },
   {
     "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
     version = false,
     dependencies = {
+      "hrsh7th/cmp-nvim-lua",
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-calc",
       "L3MON4D3/LuaSnip",
+      {
+        "L3MON4D3/LuaSnip",
+        dependencies = {
+          {
+            "rafamadriz/friendly-snippets",
+            config = function()
+              require("luasnip.loaders.from_vscode").lazy_load()
+            end,
+          },
+        },
+        opts = {
+          history = true,
+          delete_check_events = "TextChanged",
+        },
+        keys = {
+          {
+            "<tab>",
+            function()
+              return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+            end,
+            expr = true,
+            silent = true,
+            mode = "i",
+          },
+          {
+            "<tab>",
+            function()
+              require("luasnip").jump(1)
+            end,
+            mode = "s",
+          },
+          {
+            "<s-tab>",
+            function()
+              require("luasnip").jump(-1)
+            end,
+            mode = { "i", "s" },
+          },
+        },
+      },
+      {
+        "zbirenbaum/copilot-cmp",
+        config = function()
+          require("copilot_cmp").setup()
+        end,
+      },
     },
     opts = function()
       vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
@@ -91,11 +85,33 @@ return {
           end,
         }),
         sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "path" },
-          { name = "luasnip" },
-        }, {
-          { name = "buffer" },
+          { name = "copilot", priority = 10, max_item_count = 3 },
+          { name = "nvim_lsp", priority = 10 },
+          {
+            name = "buffer",
+            priority = 7,
+            keyword_length = 5,
+            max_item_count = 10,
+            option = {
+              get_bufnrs = function()
+                local bufs = {}
+                for _, win in ipairs(vim.api.nvim_list_wins()) do
+                  bufs[vim.api.nvim_win_get_buf(win)] = true
+                end
+                return vim.tbl_keys(bufs)
+              end,
+            },
+          },
+          { name = "cmp_tabnine", priority = 7 },
+          {
+            name = "luasnip",
+            priority = 7,
+            max_item_count = 5,
+          },
+          { name = "nvim_lua", priority = 5 },
+          { name = "path", priority = 4 },
+          { name = "cmd-line" },
+          { name = "calc", priority = 3 },
         }),
         experimental = {
           ghost_text = {
@@ -103,6 +119,9 @@ return {
           },
         },
         sorting = defaults.sorting,
+        performance = {
+          max_view_entries = 100,
+        },
       }
     end,
     ---@param opts cmp.ConfigSchema
--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,21 +1,11 @@
 return {
   {
     "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
     event = "InsertEnter",
     config = function()
       require("copilot").setup({
-        suggestion = {
-          enabled = true,
-          auto_trigger = true,
-          debounce = 75,
-          keymap = {
-            accept = "<C-j>",
-            next = "<C-n]>",
-            dismiss = "<C-]>",
-          },
-        },
-        server_opts_overrides = {},
+        suggestion = { enabled = false },
+        panel = { enabled = false },
       })
     end,
   },
