14c2b415975a7f2d016b3949a7c7fd2200a7d453
son.tran <son.tran@team.enouvo.com>
2024-01-26 17:15:03 +0700

    feat(master): update telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,9 +1,10 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.5",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	keys = {
-		{ "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find files" },
-		{ "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
-	},
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.5",
+  lazy = true,
+  dependencies = { "nvim-lua/plenary.nvim" },
+  keys = {
+    { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+    { "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+  },
 }
