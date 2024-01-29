0f3645be226c34666b32bf4c2317c7153a4d37d1
son.tran <son.tran@team.enouvo.com>
2024-01-29 10:04:18 +0700

    feat(master): update key map

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,6 +2,7 @@ local function augroup(name)
   return vim.api.nvim_create_augroup("auto_group_" .. name, { clear = true })
 end
 
+-- Auto run eslint fix before save if eslint is installed
 vim.api.nvim_create_autocmd("BufWritePre", {
   group = augroup("eslint"),
   callback = function()
--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -75,9 +75,6 @@ end)
 -- Diagnostic
 map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
 
--- Redo
-map("n", "<C-r>", "<Cmd>redo<cr>", otps)
-
 -- Rename
 map("n", "<leader>cr", "<Cmd>Lspsaga rename<cr>", opts)
 
