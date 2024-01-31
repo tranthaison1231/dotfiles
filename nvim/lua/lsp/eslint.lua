858b7c1dfbbab2e2a67685bf51aa7c9cd4a66d15
son.tran <son.tran@team.enouvo.com>
2024-01-31 11:56:44 +0700

    feat(master): update eslint fix all when save

--- nvim/lua/lsp/eslint.lua
+++ nvim/lua/lsp/eslint.lua
@@ -4,5 +4,13 @@ local on_attach = require("lsp/on_attach")
 lsp_config.eslint.setup({
   on_attach = function(client, bufnr)
     on_attach(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function()
+        if vim.fn.exists(":EslintFixAll") > 0 then
+          vim.cmd("EslintFixAll")
+        end
+      end,
+    })
   end,
 })
