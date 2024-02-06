ae1889806cd48e13367b9120243b469bb68b9db3
son.tran <son.tran@team.enouvo.com>
2024-02-06 15:43:00 +0700

    feat(master): add toggle term

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -99,3 +99,6 @@ map("n", "<leader>S", "<cmd>lua require('spectre').toggle()<cr>", { desc = "Togg
 map("n", "<leader>sp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
   desc = "Search on current file",
 })
+
+-- ToggleTerm
+map("n", "<leader>`", "<cmd>ToggleTerm<cr>", { desc = "Toggle terminal" })
