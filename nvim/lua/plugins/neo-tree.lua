cb910676a38ebe2a7e4cd80e9e059689942207e0
son.tran <son.tran@team.enouvo.com>
2024-02-06 16:29:18 +0700

    feat(master): auto open neotree

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,3 +1,16 @@
+vim.api.nvim_create_augroup("neotree_autoopen", { clear = true })
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  desc = "Open neo-tree on enter",
+  group = "neotree_autoopen",
+  callback = function()
+    if not vim.g.neotree_opened then
+      vim.cmd("ToggleTerm")
+      vim.cmd("Neotree show")
+      vim.g.neotree_opened = true
+    end
+  end,
+})
+
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
