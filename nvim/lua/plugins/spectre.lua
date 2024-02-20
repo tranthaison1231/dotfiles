659fc346ae8db1e323fca371b5fc674b3320b970
son.tran <son.tran@team.enouvo.com>
2024-02-20 17:45:20 +0700

    feat(master): update spectre

--- nvim/lua/plugins/spectre.lua
+++ nvim/lua/plugins/spectre.lua
@@ -15,6 +15,13 @@ return {
           search = "SpectreSearch",
           replace = "SpectreReplace",
         },
+        mapping = {
+          ["send_to_qf"] = {
+            map = "<C-q>",
+            cmd = "<cmd>lua require('spectre.actions').send_to_qf()<CR>",
+            desc = "send all items to quickfix",
+          },
+        },
       })
     end,
   },
