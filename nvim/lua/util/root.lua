132c71fdf128869d2b989a6c3f5a210697439f93
son.tran <son.tran@team.enouvo.com>
2024-02-24 14:23:02 +0700

    feat(master): remove unnecessry function

--- nvim/lua/util/root.lua
+++ nvim/lua/util/root.lua
@@ -1,31 +1,5 @@
 local M = {}
 
-function M.fg(name)
-  ---@type {foreground?:number}?
-  ---@diagnostic disable-next-line: deprecated
-  local hl = vim.api.nvim_get_hl and vim.api.nvim_get_hl(0, { name = name }) or vim.api.nvim_get_hl_by_name(name, true)
-  ---@diagnostic disable-next-line: undefined-field
-  local fg = hl and (hl.fg or hl.foreground)
-  return fg and { fg = string.format("#%06x", fg) } or nil
-end
-
-function M.get_clients(opts)
-  local ret = {} ---@type lsp.Client[]
-  if vim.lsp.get_clients then
-    ret = vim.lsp.get_clients(opts)
-  else
-    ---@diagnostic disable-next-line: deprecated
-    ret = vim.lsp.get_active_clients(opts)
-    if opts and opts.method then
-      ---@param client lsp.Client
-      ret = vim.tbl_filter(function(client)
-        return client.supports_method(opts.method, { bufnr = opts.bufnr })
-      end, ret)
-    end
-  end
-  return opts and opts.filter and vim.tbl_filter(opts.filter, ret) or ret
-end
-
 function M.is_inside_git_repo()
   local git_dir = vim.fn.finddir(".git", vim.fn.expand("%:p:h") .. ";")
   return vim.fn.isdirectory(git_dir) ~= 0
