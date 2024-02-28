945b4a8bece62cc3c683d01427450a8867d9d67c
son.tran <son.tran@team.enouvo.com>
2024-02-28 11:27:06 +0700

    feat(master): update keyword to delete without copy

--- nvim/lua/mappings/main.lua
+++ nvim/lua/mappings/main.lua
@@ -1,7 +1,7 @@
 local map = vim.keymap.set
 
 map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
-map({ "n", "v" }, "<leader>d", '"_d', { desc = "Delete without copy" })
+map({ "n", "v" }, "<leader>d", '"_dd', { desc = "Delete without copy" })
 map("n", "<leader>l", "<cmd>noh<CR>", { desc = "Clear highlights" })
 -- Disable arrow keys
 map("n", "<Up>", "<NOP>")
