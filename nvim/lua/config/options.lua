66c9b6b4ba17d20299c372826b9e28b63558d4c5
son.tran <son.tran@team.enouvo.com>
2024-02-29 23:59:01 +0700

    feat(master): update diagnosticsign

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,6 +1,18 @@
 vim.g.mapleader = " "
 vim.g.autoformat = true
 
+local signs = {
+  Error = " ",
+  Warn = " ",
+  Hint = " ",
+  Info = " ",
+}
+
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
 local opt = vim.opt
 
 opt.autowrite = true -- Enable auto write
