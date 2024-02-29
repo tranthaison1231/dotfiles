5127df9bc5c8f7b4b58604c4008b6309dd03eaa8
son.tran <son.tran@team.enouvo.com>
2024-02-29 23:44:49 +0700

    feat(master): add statuscol

--- nvim/lua/plugins/nvim-ufo.lua
+++ nvim/lua/plugins/nvim-ufo.lua
@@ -1,11 +1,34 @@
 return {
   {
     "kevinhwang91/nvim-ufo",
-    dependencies = "kevinhwang91/promise-async",
+    dependencies = {
+      "kevinhwang91/promise-async",
+      {
+        "luukvbaal/statuscol.nvim",
+        config = function()
+          local builtin = require("statuscol.builtin")
+          require("statuscol").setup({
+            relculright = true,
+            segments = {
+              { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+              {
+                sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true },
+                click = "v:lua.ScSa",
+              },
+              { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
+              {
+                sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
+                click = "v:lua.ScSa",
+              },
+            },
+          })
+        end,
+      },
+    },
     config = function()
       require("ufo").setup({
         provider_selector = function()
-          return { "lsp", "indent" }
+          return { "treesitter", "indent" }
         end,
       })
     end,
