9097758368f449eddd127fd57d8a1a7c7719293d
son.tran <son.tran@team.enouvo.com>
2024-03-01 21:41:04 +0700

    feat(master): update lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -18,15 +18,32 @@ return {
         lualine_a = {
           { "mode", width = 3 },
         },
-        lualine_b = { "branch" },
+        lualine_b = {
+          {
+            "branch",
+            on_click = function()
+              vim.cmd(":Telescope git_branches")
+            end,
+          },
+        },
         lualine_c = {
           { "diagnostics" },
           { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-          { "%f" },
+          {
+            "%f",
+            on_click = function()
+              vim.fn.setreg("*", vim.fn.expand("%:~"))
+            end,
+          },
         },
         lualine_x = {
           "fileformat",
-          "diff",
+          {
+            "diff",
+            on_click = function()
+              vim.cmd(":DiffviewOpen")
+            end,
+          },
         },
         lualine_y = {
           { "progress", separator = " ", padding = { left = 1, right = 1 } },
