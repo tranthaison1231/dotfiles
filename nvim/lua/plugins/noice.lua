e39858fa58c0932f8871985bcc7c1e918e42bd3a
son.tran <son.tran@team.enouvo.com>
2024-03-01 13:44:05 +0700

    feat(master): update noice

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -21,6 +21,19 @@ return {
           ["cmp.entry.get_documentation"] = true,
         },
       },
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "%d+L, %d+B" },
+              { find = "; after #%d+" },
+              { find = "; before #%d+" },
+            },
+          },
+          view = "mini",
+        },
+      },
       presets = {
         bottom_search = true,
         command_palette = true,
