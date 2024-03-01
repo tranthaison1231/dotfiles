271d9679ea97d87fb4536b3f7a2ccf1e0df886b6
son.tran <son.tran@team.enouvo.com>
2024-03-01 23:07:08 +0700

    feat(master): add spectre

--- nvim/lua/mappings/spectre.lua
+++ nvim/lua/mappings/spectre.lua
@@ -1,6 +1,15 @@
 local map = vim.keymap.set
 
 map("n", "<leader>S", "<cmd>lua require('spectre').toggle()<cr>", { desc = "Toggle Spectre" })
+
+map("n", "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+  desc = "Search current word",
+})
+
+map("v", "<leader>sw", '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+  desc = "Search current word",
+})
+
 map("n", "<leader>sp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
   desc = "Search on current file",
 })
